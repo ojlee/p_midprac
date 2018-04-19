ce0e1266cec7b65acea36c5fae2d169b95e7b9cf
unknown <dldhwn56@gmail.com>
2018-04-19 21:05:17 +0900

    12번

--- src/main/java/kr/ac/jejunu/JdbcContext.java
+++ src/main/java/kr/ac/jejunu/JdbcContext.java
@@ -1,10 +1,7 @@
 package kr.ac.jejunu;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcContext {
     final DataSource dataSource;
@@ -120,5 +117,40 @@ public class JdbcContext {
                     e.printStackTrace();
                 }
         }
+    }
+
+    void update(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for(int i =0; i < params.length; i++){
+                preparedStatement.setObject(i+1, params);
+            }
+            return preparedStatement;
+        };
+        jdbcContextForUpdate(statementStrategy);
+    }
+
+    long insert(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+                                //preparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for(int i =0; i < params.length; i++){
+                preparedStatement.setObject(i+1, params);
+            }
+            return preparedStatement;
+        };
+        return jdbcContextForInsert(statementStrategy);
+    }
+
+    Product queryForObject(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {//메소드를 하나 implement하는 경우에 대해서의 람다표현식 콜백패턴
+            //preparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for(int i=0; i<params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+                return preparedStatement;
+        };
+        return jdbcContextForGet(statementStrategy);
     }
 }
--- src/main/java/kr/ac/jejunu/ProductDao.java
+++ src/main/java/kr/ac/jejunu/ProductDao.java
@@ -10,48 +10,27 @@ public class ProductDao {
     }
 
     public Product get(Long id) throws SQLException {
-        StatementStrategy statementStrategy = connection -> {//메소드를 하나 implement하는 경우에 대해서의 람다표현식 콜백패턴
-            //preparedStatement
-                PreparedStatement preparedStatement = connection.prepareStatement(
-                        "SELECT * FROM product WHERE id = ?");
-                preparedStatement.setLong(1,id);
-                return preparedStatement;
-        };
-        return jdbcContext.jdbcContextForGet(statementStrategy);
+        String sql = "SELECT * FROM product WHERE id = ?";
+        Object[] params = new Object[]{id};
+        return jdbcContext.queryForObject(sql, params);
     }
 
     public long insert(Product product) throws SQLException {
-        StatementStrategy statementStrategy = connection -> {
-                                //preparedStatement
-                PreparedStatement preparedStatement = connection.prepareStatement(
-                        "INSERT INTO product(title, price) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-                preparedStatement.setString(1, product.getTitle());
-                preparedStatement.setInt(2, product.getPrice());
-            return preparedStatement;
-        };
-        return jdbcContext.jdbcContextForInsert(statementStrategy);
+        String sql = "INSERT INTO product(title, price) VALUES (?,?)";
+        Object[] params = new Object[]{product.getTitle(), product.getPrice()};
+        return jdbcContext.insert(sql, params);
     }
 
     public void update(Product product) throws SQLException {
-        StatementStrategy statementStrategy = connection -> {
-                PreparedStatement preparedStatement = connection.prepareStatement(
-            "UPDATE product SET title = ?, price = ? where id = ?");
-                preparedStatement.setString(1, product.getTitle());
-                preparedStatement.setInt(2, product.getPrice());
-                preparedStatement.setLong(3, product.getId());
-            return preparedStatement;
-        };
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        String sql = "UPDATE product SET title = ?, price = ? where id = ?";
+        Object[] params = new Object[] {product.getTitle(), product.getPrice(), product.getId()};
+        jdbcContext.update(sql, params);
     }
 
     public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = connection -> {
-            //preparedStatement
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "DELETE  FROM  product WHERE id = ?");
-            preparedStatement.setLong(1, id);
-            return preparedStatement;
-        };
-        jdbcContext.jdbcContextForUpdate(statementStrategy);
+        String sql = "DELETE  FROM  product WHERE id = ?";
+        Object[] params = new Object[]{id};
+        jdbcContext.update(sql, params);
     }
+
 }
